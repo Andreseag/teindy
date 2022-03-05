a1b7e078dff79a673daf62a4b697f189aeed0572
jaleman <javiersettex@gmail.com>
2022-03-05 15:30:58 -0500

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 const App = () => {
-  const nuevaVariable = 'Holis depronto esto va a generar un conflicto'
+  let str = 'hola mundo'
+
   return (
     <div className="App">
-      <h1>Hola mundoooo</h1>
+      <h1>Hola mundoooo {str} </h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia provident dolorum laudantium doloribus dignissimos atque, ratione modi autem commodi alias, porro repellat nesciunt, vero soluta voluptatibus tempore dolores nobis odio?</p>
     </div>
   )
