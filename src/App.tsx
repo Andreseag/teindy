a23c834ee84f59ececc4847dec714d43b7d8ce10
ac-neivor <andresc@neivor.com>
2022-03-06 22:31:56 -0500

    finish install tailwind and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-const App = () => {
-  let strNuevo = 'hola mundo'
+import './index.css'
 
+const App = () => {
   const submit = () => {
     console.log('Holiwis')
   }
@@ -8,12 +8,12 @@ const App = () => {
   return (
     <div className="App">
       <h2>Holis nueva rama</h2>
-      <h1>Hola mundoooo {str} </h1>
+      <h1>Hola mundoooo </h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia provident dolorum laudantium doloribus dignissimos atque, ratione modi autem commodi alias, porro repellat nesciunt, vero soluta voluptatibus tempore dolores nobis odio?</p>
       <div>
         <form action="">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, sapiente.</p>
-          <button onClick={submit}>Holis</button>
+          <button className='bg-cyan-500 shadow-lg' onClick={submit}>Subscribe</button>
         </form>
       </div>
     </div>
